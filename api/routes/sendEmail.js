caaab138a83d3c748ed97e63cfae3e422741aff7
rohanpawar330 <rohanpawar330@gmail.com>
2020-11-15 11:43:01 +0530

    Add files via upload

---
+++ api/routes/sendEmail.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+var nodemailer = require('nodemailer');
+
+
+
+router.post('/', (req, res, next) => {
+    const userDetail = {
+        name: req.body.name,
+        email: req.body.email,
+        emailFrom: req.body.emailFrom,
+        emailSubject: req.body.emailSubject,
+        weddingDate: req.body.weddingDate
+    };
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        // host: 'smtp.gmail.com',
+        // port: 465,
+        // secureConnection: false,
+        // secure: true,
+        // ignoreTLS: false,
+        // requireTLS: true,
+        host: 'smtp.gmail.com',
+        auth: {
+            user: userDetail.emailFrom,
+            pass: '987654321@Rp'
+        },
+        // tls: {
+        //     ciphers: 'SSLv3'
+        // }
+    });
+    var mailOptions = {
+        from: userDetail.emailFrom,
+        to: userDetail.email,
+        subject: userDetail.emailSubject,
+        // text: `Hi, Thank you for your response.`,
+
+    };
+    if (userDetail.emailFrom == 'manjeetwedskhushi@gmail.com') {
+        mailOptions.html =
+            `<h2><span style="font-size:16px">Hi</sapn> ${userDetail.name.split(" ")[0]}</h2><br>
+    <p>
+    Please join us in the celebration of our wedding,
+     which will happen on ${userDetail.weddingDate}.
+      We hope you will grant us with your presence,
+       and we would be really happy to see you on our special day.
+        All the prayers are welcome!
+    </p>
+    
+    <br>
+    <img src="https://s3.ap-south-1.amazonaws.com/www.manjeet-weds-khushi.com/images/email-img.png"/>`
+    } else {
+        mailOptions.html =
+            `<h2><span style="font-size:16px">Hi</sapn> ${userDetail.name.split(" ")[0]}</h2><br>
+<p>
+Please join us in the celebration of our wedding,
+ which will happen on ${userDetail.weddingDate}.
+  We hope you will grant us with your presence,
+   and we would be really happy to see you on our special day.
+    All the prayers are welcome!
+</p>
+
+<br>
+<img src="https://s3.ap-south-1.amazonaws.com/www.sanchit-weds-sagarika.com/images/email-img.png"/>`
+
+    }
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    res.status(200).json({
+        message: `Dear ${userDetail.name.split(" ")[0]}!,
+         Thanks for your response. Please check your email`,
+        // userDetail: userDetail
+    });
+});
+
+
+
+
+module.exports = router;
